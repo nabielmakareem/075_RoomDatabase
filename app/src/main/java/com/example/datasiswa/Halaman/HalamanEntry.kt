18608ca342f45079e7b7cd0fec52548b35bc2d56
nabielmakareem <nabielmakareem10@gmail.com>
2023-12-06 21:31:55 +0700

    add object DestinasiHome

--- app/src/main/java/com/example/datasiswa/Halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/datasiswa/Halaman/HalamanEntry.kt
@@ -1,10 +1,14 @@
 package com.example.datasiswa.Halaman
 
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.datasiswa.Model.EntryViewModel
 import com.example.datasiswa.Model.PenyediaViewModel
@@ -25,4 +29,29 @@ fun EntrySiswaScreen(
 
     val coroutineScope = rememberCoroutineScope()
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestinasiEntry.titleRes),
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior
+            )
+        }) { innerPadding ->
+        EntrySiswaBody(
+            uiStateSiswa = viewModel.UiStateSiswa,
+            onSiswaValueChange = viewModel::updateUiState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.saveSiswa()
+                    navigetBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
 }
--- app/src/main/java/com/example/datasiswa/Halaman/HalamanHome.kt
+++ app/src/main/java/com/example/datasiswa/Halaman/HalamanHome.kt
@@ -1,2 +1,9 @@
 package com.example.datasiswa.Halaman
 
+import com.example.datasiswa.Navigasi.DestinasiNavigasi
+import com.example.datasiswa.R.string.app_name
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = app_name
+}
