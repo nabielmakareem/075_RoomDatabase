db620c96102d396a79bab88d75747fdae866b3f1
nabielmakareem <nabielmakareem10@gmail.com>
2023-12-06 21:01:11 +0700

    add penyedia view model

--- app/src/main/java/com/example/datasiswa/Model/PenyediaViewModel.kt
+++ app/src/main/java/com/example/datasiswa/Model/PenyediaViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.datasiswa.AplikasiSiswa
 
 
 object PenyediaViewModel{
@@ -18,6 +19,6 @@ object PenyediaViewModel{
     /**
      * Fungsi ekstensi query untuk objek Application dan mengembalikan sebuah instance dari AplikasiSiswa
      */
-    fun CreationExtras.aplikasiSiswa():AplikasiSiswa =
+    fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
         (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY]as AplikasiSiswa)
 }
